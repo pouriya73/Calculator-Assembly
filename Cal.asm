a050762408fe89db695dfbf36531666623238de9
Pourya <cepsociety@gmail.com>
2022-05-15 12:20:36 +0300

    Create Cal.asm

---
+++ Cal.asm
@@ -0,0 +1,13 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+MSG1 DB 'Add number  :'1'$'
+MSG2 DB 10,13,'Sub number  :'2'$'
+MSG3 DB 10,13,'multiplied number   :'3'$'
+MSG4 DB 10,13,'divided number   :'4'$'
+MSG5 DB 10,13,'Press between 1 to 4:$'
+MSG6 DB 10,13,10,13,'Enter First Number:$'
+MSG7 DB 10,13,'Enter Second Number:$'
+MSG8 DB 10,13,10,13,'Calculate  :$' 
+MSG DB 10,13,10,13,'$'
+            
